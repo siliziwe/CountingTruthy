8d4b36634261fc9fec6a9d7c99689f0857286680
siliziwe <siliziwepani@gmail.com>
2022-06-20 14:38:14 +0200

    init

---
+++ code.js
@@ -0,0 +1,5 @@
+// Counting truthy
+// let booleans = [true, false, true, true];
+const arr1 = [true, true, false, true];
+const count1 = arr1.filter(value => value === true).length;
+console.log(count1);
